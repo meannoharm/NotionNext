86b95c7784ebe26c3c2371b7570eb8b583dc9fb9
stan.gao <stan.gao@tracade.cn>
2023-08-28 19:41:37 +0800

    nobelium样式修改

--- blog.config.js
+++ blog.config.js
@@ -16,13 +16,13 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Gao', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '糕糕', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一只咸鱼🐟', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://gao.tn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'czgaotian@qq.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '811460602@qq.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/meannoharm', // 你的github个人主页 例如 https://github.com/tangly1024
--- themes/nobelium/components/ArticleInfo.js
+++ themes/nobelium/components/ArticleInfo.js
@@ -1,55 +1,50 @@
-
-import Image from 'next/image'
-import BLOG from '@/blog.config'
-import TagItem from './TagItem'
-import md5 from 'js-md5'
+import Image from 'next/image';
+import BLOG from '@/blog.config';
+import TagItem from './TagItem';
+import md5 from 'js-md5';
 
 export const ArticleInfo = (props) => {
-  const { post } = props
+  const { post } = props;
 
-  const emailHash = md5(BLOG.CONTACT_EMAIL)
+  const emailHash = md5(BLOG.CONTACT_EMAIL);
 
-  return <section className="flex-wrap flex mt-2 text-gray--600 dark:text-gray-400 font-light leading-8">
-        <div>
+  return (
+    <section className="text-gray--600 mt-2 flex flex-wrap font-light leading-8 dark:text-gray-400">
+      <div>
+        <div className="text-3xl font-bold text-black dark:text-white">{post?.title}</div>
 
-            <div className="font-bold text-3xl text-black dark:text-white">
-            {post?.title}
-            </div>
-
-            {post?.type !== 'Page' && <>
-            <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
-            <div className="flex mb-4">
-              <a href={BLOG.CONTACT_GITHUB || '#'} className="flex">
-                <Image
-                  alt={BLOG.author}
-                  width={24}
-                  height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
-                  className="rounded-full"
-                />
-                <p className="ml-2 md:block">{BLOG.author}</p>
-              </a>
-              <span className="block">&nbsp;/&nbsp;</span>
-            </div>
-            <div className="mr-2 mb-4 md:ml-0">
-              {post?.publishDay}
-            </div>
-            {post?.tags && (
-              <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags">
-                {post?.tags.map(tag => (
-                  <TagItem key={tag} tag={tag} />
-                ))}
+        {post?.type !== 'Page' && (
+          <>
+            <nav className="mt-7 flex items-start text-gray-500 dark:text-gray-400">
+              <div className="mb-4 flex">
+                <a href={BLOG.CONTACT_GITHUB || '#'} className="flex">
+                  <Image
+                    alt={BLOG.AUTHOR}
+                    width={24}
+                    height={24}
+                    src={`https://gravatar.com/avatar/${emailHash}`}
+                    className="rounded-full"
+                  />
+                  <p className="ml-2 md:block">{BLOG.AUTHOR}</p>
+                </a>
+                <span className="mx-1 block">/</span>
               </div>
-            )}
-            <span className="hidden busuanzi_container_page_pv mr-2">
-                    <i className='mr-1 fas fa-eye' />
-                    &nbsp;
-                    <span className="mr-2 busuanzi_value_page_pv" />
-                </span>
-             </nav>
-            </>}
-
-        </div>
-
+              <div className="mb-4 mr-2 md:ml-0">{post?.publishDay}</div>
+              {post?.tags && (
+                <div className="article-tags mr-2 flex max-w-full flex-nowrap overflow-x-auto">
+                  {post?.tags.map((tag) => (
+                    <TagItem key={tag} tag={tag} />
+                  ))}
+                </div>
+              )}
+              <span className="busuanzi_container_page_pv mr-2 hidden">
+                <i className="fas fa-eye mr-1" />
+                <span className="busuanzi_value_page_pv mr-2" />
+              </span>
+            </nav>
+          </>
+        )}
+      </div>
     </section>
-}
+  );
+};

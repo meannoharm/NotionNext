fa22bed0cd4827a51aebd6141e101d86e07c886a
stan.gao <stan.gao@tracade.cn>
2023-08-28 17:42:07 +0800

    样式修改

--- themes/nobelium/components/Nav.js
+++ themes/nobelium/components/Nav.js
@@ -8,6 +8,7 @@ import { MenuItemDrop } from './MenuItemDrop'
 import Collapse from '@/components/Collapse'
 import { MenuItemCollapse } from './MenuItemCollapse'
 import LazyImage from '@/components/LazyImage'
+import DarkModeButton from '@/components/DarkModeButton'
 import RandomPostButton from './RandomPostButton'
 import SearchButton from './SearchButton'
 
@@ -34,42 +35,47 @@ const Nav = props => {
       if (sentinalRef.current) obvserver.unobserve(sentinalRef.current)
     }
   }, [sentinalRef])
-  return <>
-        <div className="observer-element h-4 md:h-12" ref={sentinalRef}></div>
-        <div
-            className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${!fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
-                }`}
-            id="sticky-nav"
-            ref={navRef}
-        >
-            <div className="flex items-center">
-                <Link href="/" aria-label={BLOG.title}>
-
-                    <div className="h-6 w-6">
-                        {/* <SvgIcon/> */}
-                        {CONFIG.NAV_NOTION_ICON
-                          ? <LazyImage src={siteInfo?.icon} width={24} height={24} alt={BLOG.AUTHOR} />
-                          : <SvgIcon />}
-
-                    </div>
-
-                </Link>
-                {navBarTitle
-                  ? (
-                        <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
-                            {navBarTitle}
-                        </p>
-                    )
-                  : (
-                        <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
-                            {siteInfo?.title}
-                            {/* ,{' '}<span className="font-normal">{siteInfo?.description}</span> */}
-                        </p>
-                    )}
+  return (
+    <>
+      <div className="observer-element h-4 md:h-12" ref={sentinalRef}></div>
+      <div
+        className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
+          !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
+        }`}
+        id="sticky-nav"
+        ref={navRef}
+      >
+        <div className="flex items-center">
+          <Link href="/" aria-label={BLOG.title}>
+            <div className="h-6 w-6">
+              {/* <SvgIcon/> */}
+              {CONFIG.NAV_NOTION_ICON ? (
+                <LazyImage
+                  src={siteInfo?.icon}
+                  width={24}
+                  height={24}
+                  alt={BLOG.AUTHOR}
+                />
+              ) : (
+                <SvgIcon />
+              )}
             </div>
-            <NavBar {...props} />
+          </Link>
+          {navBarTitle ? (
+            <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
+              {navBarTitle}
+            </p>
+          ) : (
+            <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
+              {siteInfo?.title}
+              {/* ,{' '}<span className="font-normal">{siteInfo?.description}</span> */}
+            </p>
+          )}
         </div>
+        <NavBar {...props} />
+      </div>
     </>
+  )
 }
 
 const NavBar = props => {
@@ -82,11 +88,37 @@ const NavBar = props => {
 
   const { locale } = useGlobal()
   let links = [
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: BLOG.ENABLE_RSS && CONFIG.MENU_RSS, target: '_blank' },
-    { icon: 'fas fa-search', name: locale.NAV.SEARCH, to: '/search', show: CONFIG.MENU_SEARCH },
-    { icon: 'fas fa-archive', name: locale.NAV.ARCHIVE, to: '/archive', show: CONFIG.MENU_ARCHIVE },
-    { icon: 'fas fa-folder', name: locale.COMMON.CATEGORY, to: '/category', show: CONFIG.MENU_CATEGORY },
-    { icon: 'fas fa-tag', name: locale.COMMON.TAGS, to: '/tag', show: CONFIG.MENU_TAG }
+    {
+      id: 2,
+      name: locale.NAV.RSS,
+      to: '/feed',
+      show: BLOG.ENABLE_RSS && CONFIG.MENU_RSS,
+      target: '_blank'
+    },
+    {
+      icon: 'fas fa-search',
+      name: locale.NAV.SEARCH,
+      to: '/search',
+      show: CONFIG.MENU_SEARCH
+    },
+    {
+      icon: 'fas fa-archive',
+      name: locale.NAV.ARCHIVE,
+      to: '/archive',
+      show: CONFIG.MENU_ARCHIVE
+    },
+    {
+      icon: 'fas fa-folder',
+      name: locale.COMMON.CATEGORY,
+      to: '/category',
+      show: CONFIG.MENU_CATEGORY
+    },
+    {
+      icon: 'fas fa-tag',
+      name: locale.COMMON.TAGS,
+      to: '/tag',
+      show: CONFIG.MENU_TAG
+    }
   ]
   if (customNav) {
     links = links.concat(customNav)
@@ -102,22 +134,42 @@ const NavBar = props => {
   }
 
   return (
-        <div className="flex-shrink-0 flex">
-            <ul className="hidden md:flex flex-row">
-                {links?.map(link => <MenuItemDrop key={link?.id} link={link} />)}
-            </ul>
-            <div className='md:hidden'>
-                <Collapse collapseRef={collapseRef} isOpen={isOpen} type='vertical' className='fixed top-16 right-6'>
-                    <div className='dark:border-black bg-white dark:bg-black rounded border p-2 text-sm'>
-                        {links?.map(link => <MenuItemCollapse key={link?.id} link={link} onHeightChange={(param) => collapseRef.current?.updateCollapseHeight(param)}/>)}
-                    </div>
-                </Collapse>
-            </div>
+    <div className="flex-shrink-0 flex">
+      <ul className="hidden md:flex flex-row">
+        {links?.map(link => (
+          <MenuItemDrop key={link?.id} link={link} />
+        ))}
+      </ul>
+      <div className="md:hidden">
+        <Collapse
+          collapseRef={collapseRef}
+          isOpen={isOpen}
+          type="vertical"
+          className="fixed top-16 right-6"
+        >
+          <div className="dark:border-black bg-white dark:bg-black rounded border p-2 text-sm">
+            {links?.map(link => (
+              <MenuItemCollapse
+                key={link?.id}
+                link={link}
+                onHeightChange={param =>
+                  collapseRef.current?.updateCollapseHeight(param)
+                }
+              />
+            ))}
+          </div>
+        </Collapse>
+      </div>
 
-            {JSON.parse(CONFIG.MENU_RANDOM_POST) && <RandomPostButton {...props} />}
-            {JSON.parse(CONFIG.MENU_SEARCH_BUTTON) && <SearchButton {...props}/>}
-            <i onClick={toggleOpen} className='fas fa-bars cursor-pointer px-5 flex justify-center items-center md:hidden'></i>
-        </div>
+      {JSON.parse(CONFIG.MENU_RANDOM_POST) && <RandomPostButton {...props} />}
+      {JSON.parse(CONFIG.MENU_SEARCH_BUTTON) && <SearchButton {...props} />}
+      <DarkModeButton />
+
+      <i
+        onClick={toggleOpen}
+        className="fas fa-bars cursor-pointer px-5 flex justify-center items-center md:hidden"
+      ></i>
+    </div>
   )
 }
 

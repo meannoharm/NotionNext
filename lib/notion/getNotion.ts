09d25de69050d79ae9bbbf656c16301cba041acb
czgaotian <811460602@qq.com>
2024-10-22 01:28:15 +0200

    type getNotion

---
+++ lib/notion/getNotion.ts
@@ -0,0 +1,56 @@
+import BLOG from '@/blog.config';
+import { idToUuid } from 'notion-utils';
+import formatDate from '../formatDate';
+import { getPostBlocks } from './getPostBlocks';
+import { defaultMapImageUrl } from 'react-notion-x';
+
+import type { Block } from './types';
+
+/**
+ * 根据页面ID获取内容
+ * @param {*} pageId
+ * @returns
+ */
+export async function getNotion(pageId: string) {
+  const blockMap = await getPostBlocks(pageId, 'slug');
+  if (!blockMap) {
+    return null;
+  }
+
+  const postInfo = blockMap?.block?.[idToUuid(pageId)].value;
+
+  return {
+    id: pageId,
+    type: postInfo,
+    category: '',
+    tags: [],
+    title: postInfo?.properties?.title?.[0],
+    status: 'Published',
+    createdTime: formatDate(
+      new Date(postInfo.created_time).toString(),
+      BLOG.LANG,
+    ),
+    lastEditedDay: formatDate(
+      new Date(postInfo?.last_edited_time).toString(),
+      BLOG.LANG,
+    ),
+    fullWidth: false,
+    page_cover: getPageCover(postInfo),
+    date: {
+      start_date: formatDate(
+        new Date(postInfo?.last_edited_time).toString(),
+        BLOG.LANG,
+      ),
+    },
+    blockMap,
+  };
+}
+
+function getPageCover(postInfo: Block) {
+  const pageCover = postInfo.format?.page_cover;
+  if (pageCover) {
+    if (pageCover.startsWith('/')) return BLOG.NOTION_HOST + pageCover;
+    if (pageCover.startsWith('http'))
+      return defaultMapImageUrl(pageCover, postInfo);
+  }
+}

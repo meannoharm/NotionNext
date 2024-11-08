a066dca5e2cb9ee136c74f4b65d209cfa38aceec
czgaotian <811460602@qq.com>
2024-11-08 23:18:11 +0100

    change blog post style

--- themes/nobelium/components/BlogPost.tsx
+++ themes/nobelium/components/BlogPost.tsx
@@ -14,9 +14,9 @@ const BlogPost: FC<BlogPostProps> = ({ post }) => {
     <Link href={`${BLOG.SUB_PATH}/${post.slug}`}>
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-          <h2 className="mb-2 cursor-pointer text-lg font-medium text-black dark:text-gray-100 md:text-xl">
+          <div className="mb-2 mr-4 cursor-pointer text-lg font-medium text-black dark:text-gray-100 md:text-xl">
             {post.title}
-          </h2>
+          </div>
           <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
             {dayjs(post?.publishDate).format('YYYY-MM-DD')}
           </time>

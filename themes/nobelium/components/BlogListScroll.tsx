df0c3d24d2d80cdc25861f12c267995989ea3b8f
czgaotian <811460602@qq.com>
2024-11-11 17:11:55 +0100

    fixed build error in blog list scroll

--- themes/nobelium/components/BlogListScroll.tsx
+++ themes/nobelium/components/BlogListScroll.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useCallback, useMemo } from 'react';
 import BLOG from '@/blog.config';
 import Link from 'next/link';
 import React from 'react';
 import { throttle } from 'lodash';
-import { deepClone } from '@/lib/utils';
 import { useTranslation } from 'next-i18next';
 
 import type { FC } from 'react';
@@ -11,52 +10,46 @@ import { PageInfo } from '@/lib/notion/types';
 
 export interface BlogListScrollProps {
   posts: PageInfo[];
-  page: number;
-  postCount: number;
 }
 
 const BlogListScroll: FC<BlogListScrollProps> = (props) => {
   const { posts } = props;
   const { t } = useTranslation('common');
-
-  const [page, updatePage] = useState(1);
-
-  let hasMore = false;
-  const postsToShow =
-    posts && Array.isArray(posts)
-      ? deepClone(posts).slice(0, BLOG.POSTS_PER_PAGE * page)
-      : [];
-
-  if (posts) {
-    const totalCount = posts.length;
-    hasMore = page * BLOG.POSTS_PER_PAGE < totalCount;
-  }
-  const handleGetMore = () => {
-    if (!hasMore) return;
-    updatePage(page + 1);
-  };
-
+  const [page, setPage] = useState(1);
   const targetRef = useRef<HTMLDivElement>(null);
 
-  // 监听滚动自动分页加载
-  useEffect(() => {
-    const scrollTrigger = throttle(() => {
-      const scrollS = window.scrollY + window.outerHeight;
-      const clientHeight = targetRef
-        ? targetRef.current
-          ? targetRef.current.clientHeight
-          : 0
-        : 0;
-      if (scrollS > clientHeight + 100) {
-        handleGetMore();
-      }
-    }, 500);
+  const hasMore = useMemo(
+    () => page * BLOG.POSTS_PER_PAGE < posts.length,
+    [posts, page],
+  );
 
-    window.addEventListener('scroll', scrollTrigger);
+  const postsToShow = useMemo(
+    () => posts.slice(0, BLOG.POSTS_PER_PAGE * page),
+    [posts, page],
+  );
+
+  const handleGetMore = useCallback(() => {
+    if (hasMore) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [hasMore]);
+
+  // 监听滚动自动分页加载
+  const scrollTrigger = useCallback(() => {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const clientHeight = targetRef.current?.clientHeight || 0;
+    if (scrollPosition > clientHeight + 100) {
+      handleGetMore();
+    }
+  }, [handleGetMore]);
+
+  useEffect(() => {
+    const scrollTriggerThrottled = throttle(scrollTrigger, 500);
+    window.addEventListener('scroll', scrollTriggerThrottled);
     return () => {
-      window.removeEventListener('scroll', scrollTrigger);
+      window.removeEventListener('scroll', scrollTriggerThrottled);
     };
-  }, []);
+  }, [scrollTrigger]);
 
   return (
     <div id="posts-wrapper" className="mb-12 w-full md:pr-12" ref={targetRef}>
@@ -72,10 +65,10 @@ const BlogListScroll: FC<BlogListScrollProps> = (props) => {
           </h2>
 
           <div className="mb-4 text-sm text-gray-700">
-            by{' '}
+            by
             <a href="#" className="text-gray-700">
               {BLOG.AUTHOR}
-            </a>{' '}
+            </a>
             on {p.date?.start_date || p.createdTime}
             <span className="mx-1 font-bold"> | </span>
             <a href="#" className="text-gray-700">
@@ -93,7 +86,6 @@ const BlogListScroll: FC<BlogListScrollProps> = (props) => {
         onClick={handleGetMore}
         className="my-4 w-full cursor-pointer py-4 text-center "
       >
-        {' '}
         {hasMore ? t('more') : `${t('no-more')} 😰`}{' '}
       </div>
     </div>

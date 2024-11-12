0680be54ea4dbc37101ae1e7874de304ffbce1a8
czgaotian <811460602@qq.com>
2024-11-12 17:07:22 +0100

    update layoutNameMapping

--- theme/useLayout.ts
+++ theme/useLayout.ts
@@ -2,6 +2,7 @@ import BLOG from '@/blog.config';
 import dynamic from 'next/dynamic';
 import { useSearchParams, usePathname } from 'next/navigation';
 import * as ThemeComponents from '@theme-components';
+import { useRouter } from 'next/router';
 
 /**
  * Route path-to-layout mapping
@@ -14,22 +15,22 @@ const layoutNameMapping: Record<string, string> = {
   '/category/[category]': 'CategoryDetail',
   '/category/[category]/page/[page]': 'CategoryPage',
   '/tag': 'Tag',
-  '/tag/[tag]': 'PostList',
-  '/tag/[tag]/page/[page]': 'PostList',
+  '/tag/[tag]': 'TagDetail',
+  '/tag/[tag]/page/[page]': 'TagPage',
   '/search': 'Search',
-  '/search/[keyword]': 'Search',
-  '/search/[keyword]/page/[page]': 'Search',
+  '/search/[keyword]': 'SearchDetail',
+  '/search/[keyword]/page/[page]': 'SearchPage',
   '/[prefix]': 'Prefix',
   '/[prefix]/[slug]': 'PrefixSlug',
   '/404': 'PageNotFound',
 };
 
 export const useLayout = (): React.ComponentType => {
-  const pathname = usePathname();
+  const router = useRouter();
   const searchParams = useSearchParams();
 
   const theme = searchParams.get('theme') || BLOG.THEME;
-  const layoutName = layoutNameMapping[pathname];
+  const layoutName = layoutNameMapping[router.pathname];
 
   return theme !== BLOG.THEME
     ? dynamic(() => import(`@/themes/${theme}`).then((m) => m[layoutName]), {

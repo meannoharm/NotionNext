679e8d26d48097edbb272668ffdea9d99d8bae07
czgaotian <811460602@qq.com>
2024-11-16 13:09:28 +0100

    change nav file structure

--- themes/nobelium/components/Nav/Nav.tsx
+++ themes/nobelium/components/Nav/Nav.tsx
@@ -1,23 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import Link from 'next/link';
 import BLOG from 'blog.config';
 import CONFIG from '../../theme.config';
 import { SvgIcon } from '../SvgIcon';
-import { MenuItemDrop } from '../MenuItemDrop';
-import Collapse from '@/components/Collapse';
-import { MenuItemCollapse } from '../MenuItemCollapse';
 import LazyImage from '@/components/LazyImage';
-import DarkModeButton from '../DarkModeButton';
-import RandomPostButton from '../RandomPostButton';
-import SearchButton from '../SearchButton';
-import LanguageSwitchButton from '../LanguageSwitchButton';
-import useToggleClickOutSide from '@/hooks/useToggleClickOutSide';
 import { useNobeliumStore } from '../../providers';
+import NavBar from '../NavBar';
 import styles from './Nav.module.css';
 
 import type { FC } from 'react';
-import type { NavLink } from '../../types';
-import type { CollapseHandle } from '@/components/Collapse';
 
 const Nav: FC = () => {
   const navRef = useRef<HTMLDivElement>(null);
@@ -79,101 +70,6 @@ const Nav: FC = () => {
         <NavBar />
       </div>
     </>
-  );
-};
-
-const NavBar: FC = () => {
-  const [isOpen, changeOpen] = useState(false);
-  const collapseRef = useRef<CollapseHandle>(null);
-  const mobileMenuRef = useRef(null);
-  const mobileMenuToggleButtonRef = useRef(null);
-
-  const toggleOpen = () => {
-    changeOpen(!isOpen);
-  };
-
-  useToggleClickOutSide([mobileMenuRef, mobileMenuToggleButtonRef], () => {
-    changeOpen(false);
-  });
-
-  const links: NavLink[] = [
-    {
-      id: 'rss',
-      icon: 'fas fa-rss',
-      name: 'rss',
-      to: '/feed',
-      show: !!(BLOG.ENABLE_RSS && CONFIG.MENU_RSS),
-      target: '_blank',
-    },
-    {
-      id: 'search',
-      icon: 'fas fa-search',
-      name: 'search',
-      to: '/search',
-      show: CONFIG.MENU_SEARCH,
-    },
-    {
-      id: 'archive',
-      icon: 'fas fa-archive',
-      name: 'archive',
-      to: '/archive',
-      show: CONFIG.MENU_ARCHIVE,
-    },
-    {
-      id: 'category',
-      icon: 'fas fa-folder',
-      name: 'category',
-      to: '/category',
-      show: CONFIG.MENU_CATEGORY,
-    },
-    {
-      id: 'tags',
-      icon: 'fas fa-tag',
-      name: 'tags',
-      to: '/tag',
-      show: CONFIG.MENU_TAG,
-    },
-  ];
-
-  return (
-    <div className="flex flex-shrink-0">
-      <ul className="hidden flex-row md:flex">
-        {links.map((link) => (
-          <MenuItemDrop key={link?.id} link={link} />
-        ))}
-      </ul>
-
-      {CONFIG.MENU_RANDOM_POST && <RandomPostButton />}
-      {CONFIG.MENU_SEARCH_BUTTON && <SearchButton />}
-      {CONFIG.MENU_LANGUAGE_SWITCH && <LanguageSwitchButton />}
-      {CONFIG.MENU_DARK_MODE_SWITCH && <DarkModeButton />}
-
-      {/* 移动端菜单按钮 */}
-      <div className="relative md:hidden">
-        <div
-          onClick={toggleOpen}
-          ref={mobileMenuToggleButtonRef}
-          className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-gray-800 hover:bg-black hover:bg-opacity-10 dark:text-gray-200 dark:hover:bg-white dark:hover:bg-opacity-10"
-        >
-          <i className="fas fa-bars"></i>
-        </div>
-
-        <div
-          ref={mobileMenuRef}
-          className={`${isOpen ? 'block' : 'hidden'} absolute right-0 w-40 rounded border border-gray-100 bg-white drop-shadow-lg transition-all duration-300 dark:border-gray-800 dark:bg-black`}
-        >
-          <Collapse collapseRef={collapseRef} isOpen={isOpen} type="vertical">
-            {links.map((link) => (
-              <MenuItemCollapse
-                key={link.id}
-                link={link}
-                onHeightChange={collapseRef.current?.updateCollapseHeight}
-              />
-            ))}
-          </Collapse>
-        </div>
-      </div>
-    </div>
   );
 };
 

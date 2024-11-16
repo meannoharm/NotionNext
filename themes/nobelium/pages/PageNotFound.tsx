51081d35479fc429834a74d950f01901488d4a77
czgaotian <811460602@qq.com>
2024-11-16 14:34:53 +0100

    update 404 page

--- themes/nobelium/pages/PageNotFound.tsx
+++ themes/nobelium/pages/PageNotFound.tsx
@@ -1,7 +1,8 @@
 import LayoutBase from '../layout/LayoutBase';
 import { ContextWrapper } from '../providers';
+import { useTranslation } from 'next-i18next';
 
-import type { FC } from 'react';
+import { type FC } from 'react';
 import type { ThemePageNotFoundProps } from '@/types';
 /**
  * 404 页面
@@ -9,7 +10,16 @@ import type { ThemePageNotFoundProps } from '@/types';
  * @returns
  */
 const PageNotFound: FC<ThemePageNotFoundProps> = (props) => {
-  return <LayoutBase {...props}>404 Not found.</LayoutBase>;
+  const { t } = useTranslation('common');
+  return (
+    <LayoutBase {...props}>
+      <div className="flex flex-col items-center">
+        <div className="mb-8 text-9xl">404</div>
+        <div className="mb-2">{t('404')}</div>
+        <div>{t('404-tips')}</div>
+      </div>
+    </LayoutBase>
+  );
 };
 
 export default ContextWrapper(PageNotFound);

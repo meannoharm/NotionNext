cf255096ef824b85a26ac20edeb3f50d8cdba136
czgaotian <811460602@qq.com>
2024-11-18 01:03:06 +0100

    update sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,12 @@
-const BLOG = require('./blog.config')
+const BLOG = require('./blog.config');
 
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: BLOG.LINK,
   changefreq: 'daily',
   priority: 0.7,
   generateRobotsTxt: true,
-  sitemapSize: 7000
+  sitemapSize: 7000,
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
-}
+};

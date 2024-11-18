76e38453d768244e7fca40601cde051627982792
czgaotian <811460602@qq.com>
2024-11-18 01:03:06 +0100

    update sitemap.xml

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,49 +1,52 @@
-// pages/sitemap.xml.js
 import { getServerSideSitemap } from 'next-sitemap';
 import { getGlobalData } from '@/lib/notion/getNotionData';
+import dayjs from 'dayjs';
 import BLOG from 'blog.config';
+
+import type { ISitemapField } from 'next-sitemap';
 
 export const getServerSideProps = async (ctx) => {
   const { allPages } = await getGlobalData('rss');
-  const defaultFields = [
+  const lastmod = dayjs().format('YYYY-MM-DD');
+  const defaultFields: ISitemapField[] = [
     {
       loc: `${BLOG.LINK}`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
     {
       loc: `${BLOG.LINK}/archive`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
     {
       loc: `${BLOG.LINK}/category`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
     {
       loc: `${BLOG.LINK}/feed`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
     {
       loc: `${BLOG.LINK}/search`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
     {
       loc: `${BLOG.LINK}/tag`,
-      lastmod: new Date().toISOString().split('T')[0],
+      lastmod,
       changefreq: 'daily',
-      priority: '0.7',
+      priority: 0.7,
     },
   ];
-  const postFields = allPages
+  const postFields: ISitemapField[] = allPages
     ?.filter((p) => p.status === BLOG.NOTION_PROPERTY_NAME.status_publish)
     ?.map((post) => {
       const slugWithoutLeadingSlash = post?.slug.startsWith('/')
@@ -51,9 +54,9 @@ export const getServerSideProps = async (ctx) => {
         : post.slug;
       return {
         loc: `${BLOG.LINK}/${slugWithoutLeadingSlash}`,
-        lastmod: new Date(post?.publishDay).toISOString().split('T')[0],
+        lastmod: dayjs(post.publishDate).format('YYYY-MM-DD'),
         changefreq: 'daily',
-        priority: '0.7',
+        priority: 0.7,
       };
     });
   const fields = defaultFields.concat(postFields);
@@ -67,4 +70,4 @@ export const getServerSideProps = async (ctx) => {
   return getServerSideSitemap(ctx, fields);
 };
 
-export default () => {};
+export default function SitemapIndex() {}

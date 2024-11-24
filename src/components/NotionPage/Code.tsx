f92ff21a9c1243a78c50f4a8e3a0c19b909e9e10
czgaotian <811460602@qq.com>
2024-11-24 14:12:29 +0100

    chore: change Code component file name

--- src/components/NotionPage/Code.tsx
+++ src/components/NotionPage/Code.tsx
@@ -1,33 +1,80 @@
-import { CodeBlock } from '@/types/notion';
+import 'prismjs';
+import 'prismjs/plugins/autoloader/prism-autoloader';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
+import 'prismjs/plugins/show-language/prism-show-language';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import Prism from 'prismjs';
+import { getBlockTitle } from 'notion-utils';
+import { useNotionContext, Text } from 'react-notion-x';
+import { useEffect, useRef } from 'react';
 import BLOG from 'blog.config';
-import Script from 'next/script';
-import { Code as CodeRender } from 'react-notion-x/build/third-party/code';
 
-import type { FC } from 'react';
+import type { CodeBlock } from 'notion-types';
 
-const Code: FC<{
+const CDN = BLOG.CDN;
+Prism.plugins.autoloader.languages_path = `https://${CDN}/prism/1.29.0/components/`;
+
+function Code({
+  block,
+  defaultLanguage = 'typescript',
+  className = '',
+}: {
   block: CodeBlock;
   defaultLanguage?: string;
   className?: string;
-}> = (props) => {
-  const CDN = BLOG.CDN;
-  const prismJsAutoLoader = `https://${CDN}/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js`;
-  const languagesPath = `https://${CDN}/prism/1.29.0/components/`;
+}) {
+  const { recordMap } = useNotionContext();
+  const content = getBlockTitle(block, recordMap);
+  const language = (() => {
+    const languageNotion = (
+      block.properties?.language?.[0]?.[0] || defaultLanguage
+    ).toLowerCase();
 
-  const handleLoad = () => {
-    if (window.Prism.plugins.autoloader) {
-      window.Prism.plugins.autoloader.languages_path = languagesPath;
+    switch (languageNotion) {
+      case 'c++':
+        return 'cpp';
+      case 'f#':
+        return 'fsharp';
+      default:
+        return languageNotion;
     }
-    Prism.highlightAll();
-  };
+  })();
+  const caption = block.properties.caption;
+
+  const codeRef = useRef();
+  useEffect(() => {
+    if (codeRef.current) {
+      try {
+        Prism.highlightElement(codeRef.current);
+      } catch (err) {
+        console.warn('prismjs highlight error', err);
+      }
+    }
+  }, [codeRef]);
 
   return (
     <>
-      <CodeRender {...props} />
-      <Script src={prismJsAutoLoader} onLoad={handleLoad} />
+      <div className="w-full">
+        <pre
+          className={`notion-code line-numbers language-${language} ${className}`}
+          style={{
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          <code ref={codeRef as any}>{content}</code>
+        </pre>
+      </div>
+
+      {caption && (
+        <figcaption className="notion-asset-caption">
+          <Text value={caption} block={block} />
+        </figcaption>
+      )}
     </>
   );
-};
+}
 
 export default Code;

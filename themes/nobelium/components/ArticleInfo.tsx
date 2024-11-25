41bee19763e684ea98e60f339433c72ace0df0a8
czgaotian <811460602@qq.com>
2024-11-25 01:01:52 +0100

    Article style

--- themes/nobelium/components/ArticleInfo.tsx
+++ themes/nobelium/components/ArticleInfo.tsx
@@ -6,6 +6,7 @@ import dayjs from 'dayjs';
 
 import type { FC } from 'react';
 import type { Page } from '@/types';
+import Link from 'next/link';
 
 export interface ArticleInfoProps {
   post: Page;
@@ -15,42 +16,40 @@ export const ArticleInfo: FC<ArticleInfoProps> = ({ post }) => {
   const emailHash = md5(BLOG.CONTACT_EMAIL);
 
   return (
-    <section className="text-gray--600 mt-2 flex flex-wrap font-light leading-8 dark:text-gray-400">
-      <div>
-        <div className="text-3xl font-bold text-black dark:text-white">
-          {post?.title}
-        </div>
+    <section className="mt-2 flex flex-col font-light">
+      <div className="text-3xl font-bold text-black dark:text-white">
+        {post?.title}
+      </div>
 
-        {post?.type !== 'Page' && (
-          <nav className="mt-7 flex items-start text-gray-500 dark:text-gray-400">
-            <div className="mb-4 flex">
-              <a href={BLOG.CONTACT_GITHUB || '#'} className="flex">
-                <Image
-                  alt={BLOG.AUTHOR}
-                  width={24}
-                  height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
-                  className="rounded-full"
-                />
-                <p className="ml-2 md:block">{BLOG.AUTHOR}</p>
-              </a>
-              <span className="mx-1 block">/</span>
-            </div>
-            <div className="mb-4 mr-2 md:ml-0">
+      {post?.type !== 'Page' && (
+        <>
+          <div className="mt-7 flex items-center text-gray-500 dark:text-gray-500">
+            <Link href={BLOG.CONTACT_GITHUB || '#'} className="mr-4">
+              <Image
+                alt={BLOG.AUTHOR}
+                width={24}
+                height={24}
+                src={`https://gravatar.com/avatar/${emailHash}`}
+              />
+            </Link>
+            <div className="mr-2 md:block">{BLOG.AUTHOR}</div>
+            <div className="mr-2 md:ml-0">
               {dayjs(post?.date).format('YYYY-MM-DD')}
             </div>
+            <span className="busuanzi_container_page_pv mr-2 hidden">
+              <i className="fas fa-eye mr-1" />
+              <span className="busuanzi_value_page_pv mr-2" />
+            </span>
+          </div>
+          <div className="mt-4">
             {post?.tags && (
               <div className="article-tags mr-2 flex max-w-full flex-nowrap overflow-x-auto">
                 {post?.tags.map((tag) => <TagItem key={tag} tag={tag} />)}
               </div>
             )}
-            <span className="busuanzi_container_page_pv mr-2 hidden">
-              <i className="fas fa-eye mr-1" />
-              <span className="busuanzi_value_page_pv mr-2" />
-            </span>
-          </nav>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };

0ad56e6920ce6c3900b07f4c5da04dc785bf3d9e
czgaotian <811460602@qq.com>
2024-11-25 16:45:59 +0100

    update nobelium theme article info style

--- themes/nobelium/components/ArticleInfo.tsx
+++ themes/nobelium/components/ArticleInfo.tsx
@@ -3,8 +3,10 @@ import BLOG from 'blog.config';
 import TagItem from './TagItem';
 import md5 from 'js-md5';
 import dayjs from 'dayjs';
+import { useNobeliumStore } from '@themes/nobelium/providers';
+import { useCallback } from 'react';
 
-import type { FC } from 'react';
+import { type FC } from 'react';
 import type { Page } from '@/types';
 import Link from 'next/link';
 
@@ -14,6 +16,13 @@ export interface ArticleInfoProps {
 
 export const ArticleInfo: FC<ArticleInfoProps> = ({ post }) => {
   const emailHash = md5(BLOG.CONTACT_EMAIL);
+  const tagOptions = useNobeliumStore((s) => s.tags);
+  const tagColor = useCallback(
+    (tag: string) => {
+      return tagOptions.find((t) => t.name === tag)?.color || 'gray';
+    },
+    [tagOptions],
+  );
 
   return (
     <section className="mt-2 flex flex-col font-light">
@@ -24,27 +33,35 @@ export const ArticleInfo: FC<ArticleInfoProps> = ({ post }) => {
       {post?.type !== 'Page' && (
         <>
           <div className="mt-7 flex items-center text-gray-500 dark:text-gray-500">
-            <Link href={BLOG.CONTACT_GITHUB || '#'} className="mr-4">
+            <Link href={BLOG.CONTACT_GITHUB || '#'} className="mr-1">
               <Image
+                className="rounded-full"
                 alt={BLOG.AUTHOR}
                 width={24}
                 height={24}
                 src={`https://gravatar.com/avatar/${emailHash}`}
               />
             </Link>
-            <div className="mr-2 md:block">{BLOG.AUTHOR}</div>
-            <div className="mr-2 md:ml-0">
+            <div className="mr-4 md:block">{BLOG.AUTHOR}</div>
+            <div className="mr-4 md:ml-0">
               {dayjs(post?.date).format('YYYY-MM-DD')}
             </div>
-            <span className="busuanzi_container_page_pv mr-2 hidden">
+            <span className="busuanzi_container_page_pv hidden">
               <i className="fas fa-eye mr-1" />
-              <span className="busuanzi_value_page_pv mr-2" />
+              <span className="busuanzi_value_page_pv" />
             </span>
           </div>
           <div className="mt-4">
             {post?.tags && (
               <div className="article-tags mr-2 flex max-w-full flex-nowrap overflow-x-auto">
-                {post?.tags.map((tag) => <TagItem key={tag} tag={tag} />)}
+                {post?.tags.map((tag) => (
+                  <TagItem
+                    className="mr-2"
+                    key={tag}
+                    tag={tag}
+                    color={tagColor(tag)}
+                  />
+                ))}
               </div>
             )}
           </div>

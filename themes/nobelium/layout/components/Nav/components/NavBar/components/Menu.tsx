c9c2c7370d079f3043141b05cb9ea0e35df82135
czgaotian <811460602@qq.com>
2024-11-26 04:21:48 +0100

    optimize nobelium nav

---
+++ themes/nobelium/layout/components/Nav/components/NavBar/components/Menu.tsx
@@ -0,0 +1,121 @@
+import Collapse from '@/components/Collapse';
+import Link from 'next/link';
+import { useCallback, useRef, useState } from 'react';
+
+import type { FC, RefObject } from 'react';
+import type { Nav } from '@/types/notion';
+import useToggleClickOutSide from '@/hooks/useToggleClickOutSide';
+
+export interface MenuProps {
+  menuList: Nav[];
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  className?: string;
+  clickOutSideClose?: boolean;
+  excludeRef?: RefObject<HTMLDivElement>;
+}
+
+/**
+ * 折叠菜单
+ * @param {*} param0
+ * @returns
+ */
+export const Menu: FC<MenuProps> = (props) => {
+  const {
+    menuList,
+    isOpen,
+    setIsOpen,
+    excludeRef,
+    clickOutSideClose = true,
+    className,
+  } = props;
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const refs = [menuRef];
+  if (excludeRef) {
+    refs.push(excludeRef);
+  }
+
+  useToggleClickOutSide(refs, () => {
+    if (clickOutSideClose) {
+      setIsOpen(false);
+    }
+  });
+
+  return (
+    <div
+      ref={menuRef}
+      className={`${isOpen ? 'block' : 'hidden'}  rounded border border-gray-100 bg-white drop-shadow transition-all duration-300 dark:border-gray-900 dark:bg-black ${className}`}
+    >
+      <MenuList menuList={menuList} isOpen={isOpen} />
+    </div>
+  );
+};
+
+const MenuList = ({
+  menuList,
+  isOpen,
+}: {
+  menuList: Nav[];
+  isOpen: boolean;
+}) => {
+  return (
+    <Collapse isOpen={isOpen}>
+      {menuList.map((menu) => {
+        if (menu.subMenus && menu.subMenus.length > 0) {
+          return <SubMenuItem key={menu.id} menu={menu} />;
+        } else {
+          return <MenuItem key={menu.id} menu={menu} />;
+        }
+      })}
+    </Collapse>
+  );
+};
+
+const MenuItem = ({ menu }: { menu: Nav }) => {
+  return (
+    <Link
+      href={menu.to}
+      className="cursor flex p-3 text-gray-800 transition-all duration-200  hover:bg-gray-200  dark:text-gray-200 dark:hover:bg-gray-800"
+    >
+      {menu.icon && (
+        <div className="mr-2">
+          <i className={menu.icon} />
+        </div>
+      )}
+      <div>{menu.title}</div>
+    </Link>
+  );
+};
+
+const SubMenuItem = ({ menu }: { menu: Nav }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => setIsOpen(!isOpen), [isOpen]);
+
+  return (
+    <>
+      <div
+        onClick={toggleOpen}
+        className="flex cursor-pointer p-3 text-gray-800 transition-all duration-200 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-800"
+      >
+        {menu?.icon && (
+          <div className="mr-2">
+            <i className={menu.icon} />
+          </div>
+        )}
+        <div>{menu.title}</div>
+        <div className="ml-auto flex items-center">
+          <i
+            className={`fas fa-chevron-down transition-all duration-200 ${isOpen ? 'rotate-180' : ''}`}
+          ></i>
+        </div>
+      </div>
+      <div className="pl-4">
+        {isOpen && menu.subMenus && (
+          <MenuList menuList={menu.subMenus} isOpen={isOpen} />
+        )}
+      </div>
+    </>
+  );
+};
---
+++ themes/nobelium/layout/components/Nav/components/NavBar/components/NavItem.tsx
@@ -0,0 +1,70 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { useTranslation } from 'next-i18next';
+import { useRef } from 'react';
+
+import type { FC } from 'react';
+import type { Nav } from '@/types/notion';
+import { Menu } from './Menu';
+
+export interface NavItemProps {
+  link: Nav;
+}
+export const NavItem: FC<NavItemProps> = ({ link }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+  const { t } = useTranslation('nav');
+
+  const toggleShow = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (!link || !link.show) {
+    return null;
+  }
+
+  const hasSubMenu = link.subMenus && link?.subMenus.length > 0;
+
+  return (
+    <div className="relative">
+      <div
+        className="cursor-pointer rounded-full px-4 py-2 text-gray-800 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-800"
+        ref={menuRef}
+      >
+        {hasSubMenu ? (
+          <div
+            className={`flex items-center text-black dark:text-gray-50`}
+            onClick={toggleShow}
+          >
+            {link?.icon && (
+              <div className="mr-2">
+                <i className={link?.icon} />
+              </div>
+            )}
+            <span className="font-medium">{link?.title}</span>
+            <i
+              className={`fas fa-chevron-down ml-2 transition-all duration-200 ${isOpen ? ' rotate-180' : ''}`}
+            ></i>
+          </div>
+        ) : (
+          <Link
+            className={`block text-black dark:text-gray-50`}
+            href={link?.to}
+          >
+            {link?.icon && <i className={link?.icon} />}{' '}
+            <span className="font-medium">{t(link.title)}</span>
+          </Link>
+        )}
+      </div>
+      {hasSubMenu && (
+        <Menu
+          className="absolute right-4 top-10 w-60"
+          menuList={link.subMenus as Nav[]}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          excludeRef={menuRef}
+        />
+      )}
+    </div>
+  );
+};

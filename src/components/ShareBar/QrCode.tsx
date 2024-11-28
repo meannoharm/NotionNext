cf0bf6a2e4e6a0ff2ccdf49c61b78585aa448616
czgaotian <811460602@qq.com>
2024-11-28 20:57:18 +0100

    feat: refractor share button

---
+++ src/components/ShareBar/QrCode.tsx
@@ -0,0 +1,46 @@
+import BLOG from 'blog.config';
+import { useEffect, useRef } from 'react';
+import Script from 'next/script';
+
+/**
+ * 二维码生成
+ */
+export default function QrCode({ value }: { value: string }) {
+  const qrcodeRef = useRef<any>(null);
+  const domRef = useRef<HTMLDivElement>(null);
+
+  const generateQrCode = () => {
+    if (!value) return;
+
+    const QRCode = (window as any).QRCode;
+    if (QRCode) {
+    qrcodeRef.current = new QRCode(domRef.current, {
+      text: value,
+      width: 256,
+      height: 256,
+      colorDark: '#000000',
+      colorLight: '#ffffff',
+      correctLevel: QRCode.CorrectLevel.H,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (domRef.current) {
+      qrcodeRef.current.clear();
+      qrcodeRef.current.makeCode(value);
+    } else {
+      generateQrCode();
+    }
+  }, [value]);
+
+  return (
+    <>
+      <div ref={domRef}></div>
+      <Script
+        src={`${BLOG.CDN}/qrcodejs/1.0.0/qrcode.min.js`}
+        onLoad={generateQrCode}
+      />
+    </>
+  );
+}

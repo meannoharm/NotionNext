b05770316dc93c5c17504824ec46f3721283b6ab
czgaotian <811460602@qq.com>
2024-11-29 13:48:28 +0100

    feat: prepare for aloglia search

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -1,12 +1,28 @@
 import BLOG from 'blog.config';
-import { getPageContentText } from '@/pages/search/[keyword]';
+import getPageContentText from '@/lib/notion/getPageContentText';
 import algoliasearch from 'algoliasearch';
+import dayjs from 'dayjs';
+
+import type { ExtendedRecordMap, Page } from '@/types/notion';
+
+export interface AlgoliaRecord {
+  objectID: string;
+  title: string;
+  category: string;
+  tags: string[];
+  pageCover: string;
+  slug: string;
+  summary: string;
+  lastEditedDate: number;
+  lastIndexDate: number;
+  content: string;
+}
 
 /**
  * 生成全文索引
  * @param {*} allPages
  */
-const generateAlgoliaSearch = async ({ allPages }) => {
+const generateAlgoliaSearch = async (allPages: Page[]) => {
   allPages?.forEach((p) => {
     // 判断这篇文章是否需要重新创建索引
     if (p && !p.password) {
@@ -19,40 +35,36 @@ const generateAlgoliaSearch = async ({ allPages }) => {
  * 上传数据
  * 根据上次修改文章日期和上次更新索引数据判断是否需要更新algolia索引
  */
-const uploadDataToAlgolia = async (post) => {
+const uploadDataToAlgolia = async (post: Page) => {
   // Connect and authenticate with your Algolia app
   const client = algoliasearch(BLOG.ALGOLIA_APP_ID, BLOG.ALGOLIA_ADMIN_APP_KEY);
 
   // Create a new index and add a record
-  const index = client.initIndex(BLOG.ALGOLIA_INDEX);
+  const algoliaIndex = client.initIndex(BLOG.ALGOLIA_INDEX);
 
   if (!post) {
     return;
   }
 
   // 检查是否有索引
-  let existed;
   let needUpdateIndex = false;
   try {
-    existed = await index.getObject(post.id);
+    const existed = await algoliaIndex.getObject<AlgoliaRecord>(post.id);
+    if (!existed || !existed?.lastEditedDate || !existed?.lastIndexDate) {
+      needUpdateIndex = true;
+    } else {
+      if (dayjs(post.lastEditedDate).isAfter(dayjs(existed.lastIndexDate))) {
+        needUpdateIndex = true;
+      }
+    }
   } catch (error) {
     console.error(error);
     // 通常是不存在索引
   }
 
-  if (!existed || !existed?.lastEditedDate || !existed?.lastIndexDate) {
-    needUpdateIndex = true;
-  } else {
-    const lastEditedDate = new Date(existed.lastEditedDate);
-    const lastIndexDate = new Date(existed.lastIndexDate);
-    if (lastEditedDate.getTime() > lastIndexDate.getTime()) {
-      needUpdateIndex = true;
-    }
-  }
-
   // 如果需要更新搜索
   if (needUpdateIndex) {
-    const record = {
+    const record: AlgoliaRecord = {
       objectID: post.id,
       title: post.title,
       category: post.category,
@@ -61,11 +73,12 @@ const uploadDataToAlgolia = async (post) => {
       slug: post.slug,
       summary: post.summary,
       lastEditedDate: post.lastEditedDate, // 更新文章时间
-      lastIndexDate: new Date(), // 更新索引时间
-      content: truncate(getPageContentText(post, post.blockMap), 9000), // 索引9000个字节，因为api限制总请求内容上限1万个字节
+      lastIndexDate: dayjs().valueOf(), // 更新索引时间
+      content: truncate(getPageContentText(post, post.blockMap as ExtendedRecordMap).join(''), 9000), // 索引9000个字节，因为api限制总请求内容上限1万个字节
     };
+
     // console.log('更新Algolia索引', record)
-    index
+    algoliaIndex
       .saveObject(record)
       .wait()
       .then((r) => {
@@ -83,7 +96,7 @@ const uploadDataToAlgolia = async (post) => {
  * @param {*} maxBytes
  * @returns
  */
-function truncate(str, maxBytes) {
+function truncate(str: string, maxBytes: number) {
   let count = 0;
   let result = '';
   for (let i = 0; i < str.length; i++) {
--- src/lib/notion/getSearchResult.ts
+++ src/lib/notion/getSearchResult.ts
@@ -1,6 +1,7 @@
-import { Page, Site } from "@/types/notion";
 import { getDataFromCache } from "../cache/cacheManager";
+import getPageContentText from "./getPageContentText";
 
+import type { ExtendedRecordMap, Page } from "@/types/notion";
 /**
  * 在内存缓存中进行全文索引
  * @param {*} posts
@@ -13,7 +14,7 @@ export default async function getSearchResult(posts: Page[], keyword: string) {
   const filterPosts: Page[] = [];
 
   posts.forEach(async (post) => {
-    const page = await getDataFromCache<Site>(`page_block_${post.id}`, true);
+    const pageBlock = await getDataFromCache<ExtendedRecordMap>(`page_block_${post.id}`, true);
     const tagContent = post?.tags?.join(' ') || '';
     const categoryContent = post?.category || '';
     const articleInfo = (
@@ -27,7 +28,7 @@ export default async function getSearchResult(posts: Page[], keyword: string) {
     let hit = articleInfo.includes(lowerKeyword);
     let hitCount = 0;
 
-    const indexContent = page ? getPageContentText(post, page) : [];
+    const indexContent = pageBlock ? getPageContentText(post, pageBlock) : '';
     for (const content of indexContent) {
       if (!content) continue;
       if (content.toLowerCase().includes(lowerKeyword)) {
@@ -44,39 +45,3 @@ export default async function getSearchResult(posts: Page[], keyword: string) {
   return filterPosts;
 }
 
-function getPageContentText(post: Page, dataBaseInfo: Site) {
-  let indexContent: string[] = [];
-  if (dataBaseInfo && dataBaseInfo?.block && !post.password) {
-    Object.values(dataBaseInfo.block).forEach((block) => {
-      const properties = block.value?.properties;
-      indexContent = extractTextContent(indexContent, properties, 'title');
-      indexContent = extractTextContent(indexContent, properties, 'caption');
-    });
-  }
-  return indexContent;
-}
-
-function extractTextContent(
-  sourceText: string[],
-  targetObj: any,
-  key: string,
-): string[] {
-  if (!targetObj) return sourceText;
-  const textArray = targetObj[key];
-  const text =
-    typeof textArray === 'object' ? getTextContent(textArray) : textArray;
-  return text && text !== 'Untitled' ? sourceText.concat(text) : sourceText;
-}
-
-/**
- * 递归获取层层嵌套的数组
- * @param {*} textArray
- * @returns
- */
-function getTextContent(textArray: any): string {
-  return Array.isArray(textArray)
-    ? textArray.reduce((acc, item) => acc + getTextContent(item), '')
-    : typeof textArray === 'string'
-      ? textArray
-      : '';
-}

1ff4f6be12e75732bbf7ea663e7cba4967f49f70
czgaotian <811460602@qq.com>
2024-12-05 16:00:54 +0100

    chore: cleaning tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,5 @@ module.exports = {
   darkMode: 'selector',
   theme: {
     fontFamily: fontFamilies,
-    extend: {
-      colors: {
-        day: {
-          DEFAULT: BLOG.BACKGROUND_LIGHT || '#ffffff',
-        },
-        night: {
-          DEFAULT: BLOG.BACKGROUND_DARK || '#111827',
-        },
-        hexo: {
-          'background-gray': '#f5f5f5',
-          'black-gray': '#101414',
-          'light-gray': '#e5e5e5',
-        },
-      },
-      maxWidth: {
-        side: '14rem',
-      },
-    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };

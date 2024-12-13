32bf99f8e3766e194d935a9b416631084ee9f1c8
czgaotian <811460602@qq.com>
2024-12-13 16:17:09 +0100

    fix: remove font serif, back in the future

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-const { fontFamilies } = require('./scripts/font');
-
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './themes/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'selector',
-  theme: {
-    fontFamily: fontFamilies,
-  },
 };

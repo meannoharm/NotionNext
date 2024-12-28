86472d9696268b7f978b4597ffaf01969a39ffc7
czgaotian <811460602@qq.com>
2024-12-28 17:00:00 +0100

    feat: update debug panel

--- components/ExternalPlugins/components/DebugPanel.tsx
+++ components/ExternalPlugins/components/DebugPanel.tsx
@@ -26,18 +26,6 @@ const DebugPanel = () => {
     setTheme(newTheme);
   }
 
-  function filterResult(text: any) {
-    switch (text) {
-      case 'true':
-        return <span className="text-green-500">true</span>;
-      case 'false':
-        return <span className="text-red-500">false</span>;
-      case '':
-        return '-';
-    }
-    return text;
-  }
-
   return (
     <>
       {/* 调试按钮 */}
@@ -48,53 +36,55 @@ const DebugPanel = () => {
           onClick={toggleShow}
         >
           {show ? (
-            <i className="fas fa-times">&nbsp;{t('debug-close')}</i>
+            <span>
+              <i className="fas fa-times mb-1"></i>
+              <span>{t('debug-close')}</span>
+            </span>
           ) : (
-            <i className="fas fa-tools">&nbsp;{t('debug-open')}</i>
+            <span>
+              <i className="fas fa-tools mb-1"></i>
+              <span>{t('debug-open')}</span>
+            </span>
           )}
         </div>
       </div>
 
       {/* 调试侧拉抽屉 */}
       <div
-        className={` ${show ? 'shadow-card right-0 w-96 ' : 'invisible -right-96 w-0'} fixed bottom-0 z-50 h-full overflow-y-scroll bg-white p-5 duration-200`}
+        className={`${show ? 'shadow-card right-0 w-96 ' : 'invisible -right-96 w-0'} fixed bottom-0 z-50 h-full overflow-y-scroll border-l bg-white p-5 shadow duration-200`}
       >
-        <div className="my-5 flex justify-between space-x-1">
-          <div className="flex">
-            <Select
-              label={t('theme-switch')}
-              value={theme}
-              options={themeList}
-              onChange={setTheme}
-            />
-            <div className="cursor-pointer p-2" onClick={randomTheme}>
-              <i className="fas fa-sync" />
-            </div>
+        <div className="mb-2 flex justify-between">
+          <Select
+            label={t('theme-switch')}
+            value={theme}
+            options={themeList}
+            onChange={setTheme}
+          />
+          <div className="cursor-pointer p-2" onClick={randomTheme}>
+            <i className="fas fa-sync" />
           </div>
-
-          <div className="p-2">
-            <i className="fas fa-times" onClick={toggleShow} />
+          <div className="ml-auto cursor-pointer p-2" onClick={toggleShow}>
+            <i className="fas fa-times" />
           </div>
         </div>
 
         <div>
-          <div className="w-18 my-2 border-b font-bold">
-            站点配置[blog.config.js]
+          <div className="w-18 mb-2 border-b pb-2 font-bold">
+            {t('site-config')}
           </div>
-          <div className="text-xs">
+          <div className="">
             {siteConfig &&
-              Object.keys(siteConfig).map((k) => (
-                <div key={k} className="flex justify-between py-1">
-                  <span className="mr-2 rounded bg-blue-500 p-0.5 text-white">
-                    {k}
-                  </span>
-                  <span className="whitespace-nowrap">
-                    {filterResult(siteConfig[k as keyof typeof siteConfig])}
-                  </span>
+              Object.entries(siteConfig).map(([key, value]) => (
+                <div key={key} className="mb-2 flex flex-col">
+                  <div className="mb-1">{key}</div>
+                  <div className="overflow-hidden border">
+                    {JSON.stringify(value)}
+                  </div>
                 </div>
               ))}
           </div>
         </div>
+        <div></div>
       </div>
     </>
   );

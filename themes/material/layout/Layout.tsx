344e4aa40277b5ddd859c6d740d32fe11fe0ab43
czgaotian <811460602@qq.com>
2024-12-30 21:27:32 +0100

    feat: add material darkmode

--- themes/material/layout/Layout.tsx
+++ themes/material/layout/Layout.tsx
@@ -3,21 +3,34 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Footer from '../components/Footer';
-
-import { type FC, type ReactNode } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useStyleStore } from '@/providers/styleProvider';
+import { useMemo, type FC, type ReactNode } from 'react';
 
 const Layout: FC<{
   children: ReactNode;
 }> = ({ children }) => {
+  const isDarkMode = useStyleStore((state) => state.isDarkMode);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: isDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [isDarkMode],
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Container maxWidth="lg">
         <Box my={4}>{children}</Box>
       </Container>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 

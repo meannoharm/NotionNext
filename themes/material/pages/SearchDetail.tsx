41467b3b39a0a1344efa98e4942a1f221fa02e98
czgaotian <811460602@qq.com>
2025-01-02 18:45:25 +0100

    feat: highlight search results

--- themes/material/pages/SearchDetail.tsx
+++ themes/material/pages/SearchDetail.tsx
@@ -5,10 +5,19 @@ import { useSiteStore } from '@/providers/siteProvider';
 import SearchInput from '../components/SearchInput';
 import Stack from '@mui/material/Stack';
 import PostList from '../components/PostLists';
+import { useEffect } from 'react';
+import markText from '@/utils/markText';
 
 const SearchDetail = () => {
   const keyword = useSiteStore((state) => state.keyword);
   const { t } = useTranslation('nav');
+
+  useEffect(() => {
+    markText('#posts-wrapper', keyword, {
+      element: 'span',
+      className: 'text-red-500 border-b border-dashed',
+    });
+  }, [keyword]);
 
   return (
     <Layout>

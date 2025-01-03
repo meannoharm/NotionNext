769a2bc05685d064f67bb026c93a8fc9098a846f
czgaotian <811460602@qq.com>
2025-01-03 11:19:21 +0100

    feat: page not found

---
+++ themes/material/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import Layout from '../layout/Layout';
+import PageNotFoundTip from '@/components/PageNotFoundTip';
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+      <PageNotFoundTip />
+    </Layout>
+  );
+};
+
+export default PageNotFound;
